21c22491dbafb6956a331ef63ff077045454b155
mike-taylorprops <mike@taylorprops.com>
2021-07-25 15:14:11 -0400

    changes

--- resources/js/admin/monitor.js
+++ resources/js/admin/monitor.js
@@ -1,7 +1,6 @@
 if (document.URL.match(/monitor/)) {
 
     jobs();
-    //setInterval(jobs, 1000);
 
 
     function jobs() {
@@ -10,6 +9,21 @@ if (document.URL.match(/monitor/)) {
                 document.querySelector('.monitor').innerHTML = response.data;
                 document.querySelector('.text-5xl').classList.add('text-3xl');
                 document.querySelector('.text-5xl').classList.remove('text-5xl');
+
+                let insert_after = document.getElementsByClassName('px-6 py-4 mb-6 pl-4');
+
+                let controllers = ' \
+                <div class="my-3 block"> \
+                    <div class="flex justify-start items-center" x-data="controls()"> \
+                        <div class="mr-2"><a href="javascript:void(0)" class="p-2 bg-gray-50 border rounded shadow control-button" x-on:click="play()"><i class="fal fa-play fa-lg text-gray-600"></i></a></div> \
+                        <div class="mr-2"><a href="javascript:void(0)" class="p-2 bg-gray-50 border rounded shadow control-button" x-on:click="stop()"><i class="fal fa-stop fa-lg text-gray-600"></i></a></div> \
+                        <div class="mr-2"><a href="javascript:void(0)" class="p-2 bg-gray-50 border rounded shadow control-button" x-on:click="refresh()"><i class="fal fa-redo fa-lg text-gray-600"></i></a></div> \
+                    </div> \
+                </div>';
+                let controllers_div = document.createElement('div');
+                controllers_div.innerHTML = controllers;
+                insertAfter(controllers_div, insert_after.item(0));
+
             })
             .catch(function (error) {
                 console.log(error);
@@ -30,6 +44,10 @@ if (document.URL.match(/monitor/)) {
                 jobs();
             }
         }
+    }
+
+    function insertAfter(newNode, existingNode) {
+        existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
     }
 
 }
