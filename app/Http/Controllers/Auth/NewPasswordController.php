33a2cb6357d1520158449cb4cf6cfd46591bd42f
mike-taylorprops <mike@taylorprops.com>
2021-07-25 21:44:58 -0400

    changes

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Str;
-//use Illuminate\Validation\Rules;
+use Illuminate\Validation\Rules;
 
 class NewPasswordController extends Controller
 {
--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -9,7 +9,7 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-//use Illuminate\Validation\Rules;
+use Illuminate\Validation\Rules;
 
 class RegisteredUserController extends Controller
 {
