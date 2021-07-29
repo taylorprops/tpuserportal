3a55c660d8b881bbf684992689ccfde3d3cc155b
mike-taylorprops <mike@taylorprops.com>
2021-07-29 14:42:56 -0400

    changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -32,7 +32,7 @@ class Kernel extends ConsoleKernel
         // add missing documents to skyslope transactions
         $schedule -> command('skyslope:add_missing_documents') -> everyHour();
         // add skyslope data to old db
-        //$schedule -> command('old_db:add_skyslope_listings') -> everyMinute();
+        $schedule -> command('old_db:add_skyslope_listings') -> everyMinute();
     }
 
     /**
