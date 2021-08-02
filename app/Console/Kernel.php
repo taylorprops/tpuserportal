dd91b0a5a56ef097b6f07ea7d81dbf36bcdf03dc
mike-taylorprops <mike@taylorprops.com>
2021-08-02 15:48:37 -0400

    changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,17 +24,29 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        // telescope error finding - clear database
         $schedule -> command('telescope:prune') -> daily();
+
+        // %%% TEMP %%% //
+
         // add transactions from skyslope to db
+        // ends - when no more transactions added to skyslope
         $schedule -> command('skyslope:get_transactions') -> everySixHours();
         // add documents to skyslope transactions
+        // ends - when no more transactions added to skyslope
         $schedule -> command('skyslope:add_documents') -> everyTenMinutes();
         // add missing documents to skyslope transactions
+        // ends - when no more transactions added to skyslope
         $schedule -> command('skyslope:add_missing_documents') -> hourly();
         // add skyslope data to old db
+        // ends - when no more transactions added to skyslope
         $schedule -> command('old_db:add_skyslope_listings') -> everyMinute();
+
         // add mls_company to skyslope
+        // ends - when all data added
         $schedule -> command('skyslope:add_mls_company_transactions') -> everyMinute();
+
+        // %%% END TEMP %%% //
     }
 
     /**
