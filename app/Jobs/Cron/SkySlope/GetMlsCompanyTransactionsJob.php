991070718fe5c039ebb412c15cbdb8409e77241b
mike-taylorprops <mike@taylorprops.com>
2021-08-02 15:56:13 -0400

    changes

--- app/Jobs/Cron/SkySlope/GetMlsCompanyTransactionsJob.php
+++ app/Jobs/Cron/SkySlope/GetMlsCompanyTransactionsJob.php
@@ -53,127 +53,130 @@ class GetMlsCompanyTransactionsJob implements ShouldQueue
         -> inRandomOrder()
         -> get();
 
-        foreach ($transactions as $transaction) {
-            $transaction -> downloaded = 'yes';
-            $transaction -> save();
-        }
+        if(count($transactions) > 0) {
 
-        $queue_data = [];
-
-        foreach ($transactions as $transaction) {
-
-            $docs = $transaction -> docs;
-
-            if(count($docs) > 0) {
-
-                $queue_data[] = $transaction -> ListingSourceRecordId;
-
-                $add_transaction = new SkySlopeTransactions();
-
-                if($transaction -> ListAgentCompID > 0) {
-                    $agent_id = $transaction -> ListAgentCompID;
-                } else {
-                    $agent_id = $transaction -> SaleAgentCompID ?? 0;
-                }
-
-                $guid = (string) Str::uuid();
-
-                $add_transaction -> objectType = $transaction -> listing;
-                $add_transaction -> listingGuid = $guid;
-                $add_transaction -> status = $transaction -> MlsStatus;
-                $add_transaction -> listingDate = $transaction -> MLSListDate;
-                $add_transaction -> expirationDate = $transaction -> ExpirationDate;
-                $add_transaction -> escrowClosingDate = $transaction -> CloseDate;
-                $add_transaction -> actualClosingDate = $transaction -> CloseDate;
-                $add_transaction -> contractAcceptanceDate = $transaction -> PurchaseContractDate;
-                $add_transaction -> listingPrice = $transaction -> ListPrice;
-                $add_transaction -> salePrice = $transaction -> ClosePrice;
-
-                $county = $transaction -> County;
-                if($county == '') {
-                    $county = $this -> county(substr($transaction -> PostalCode, 0, 5));
-                }
-                $county = str_replace(' County', '', $county);
-                $county = str_replace("'", "", $county);
-
-                $property = [
-                    'zip' => $transaction -> PostalCode,
-                    'city' => $transaction -> City,
-                    'unit' => $transaction -> UnitNumber,
-                    'state' => $transaction -> StateOrProvince,
-                    'county' => $county,
-                    'direction' => $transaction -> StreetDirPrefix.$transaction -> StreetDirSuffix,
-                    'yearBuilt' => $transaction -> YearBuilt,
-                    'streetNumber' => $transaction -> StreetNumber,
-                    'streetAddress' => $transaction -> StreetName,
-                ];
-                $add_transaction -> property = json_encode($property);
-
-                $agent = [
-                    'guid' => '',
-                    'publicId' => $agent_id
-                ];
-                $add_transaction -> agent = json_encode($agent);
-
-                $add_transaction -> mlsNumber = $transaction -> ListingSourceRecordId ?? '0';
-                $add_transaction -> data_source = 'mls_company';
-
-                $add_transaction -> save();
-
-                // add docs to db - downloading done in separate controller
-                $dir = 'doc_management/skyslope/'.$guid;
-                if(!Storage::exists($dir)) {
-                    Storage::makeDirectory($dir);
-                }
-
-                $downloads = [];
-
-                foreach($docs as $doc) {
-
-                    if($doc -> invalid != 'yes') {
-
-                        //if(file_exists($doc -> upload_loc)) {
-
-                            $downloads[] = ['from' => $doc -> upload_loc, 'to' => $dir.'/'.$doc -> upload_file_name];
-
-                            $add_docs = new SkySlopeDocuments();
-                            $add_docs -> id = (string) Str::uuid();
-                            $add_docs -> listingGuid = $guid;
-                            $add_docs -> doc_type = $doc -> doc_type;
-                            $add_docs -> name = str_replace('.pdf', '', $doc -> upload_file_name);
-                            $add_docs -> fileName = $doc -> upload_file_name;
-                            $add_docs -> extension = 'pdf';
-                            $add_docs -> url = $doc -> upload_loc;
-                            $add_docs -> file_location = $dir.'/'.$doc -> upload_file_name;
-
-                            $add_docs -> save();
-
-                        //}
-
-                    }
-
-                }
-
-                if(count($downloads) > 0) {
-
-                    $progress_increment = round((1 / count($downloads)) * 100);
-
-                    foreach($downloads as $download) {
-
-                        if(@fopen($download['from'], 'r')) {
-                            Storage::put($download['to'], file_get_contents($download['from']));
-                        }
-
-                        $progress += $progress_increment;
-                        $this -> queueProgress($progress);
-
-                    }
-
-                }
-
+            foreach ($transactions as $transaction) {
+                $transaction -> downloaded = 'yes';
+                $transaction -> save();
             }
 
-            $this -> queueData(['ListingSourceRecordIds' => $queue_data]);
+            $queue_data = [];
+
+            foreach ($transactions as $transaction) {
+
+                $docs = $transaction -> docs;
+
+                if(count($docs) > 0) {
+
+                    $queue_data[] = $transaction -> ListingSourceRecordId;
+
+                    $add_transaction = new SkySlopeTransactions();
+
+                    if($transaction -> ListAgentCompID > 0) {
+                        $agent_id = $transaction -> ListAgentCompID;
+                    } else {
+                        $agent_id = $transaction -> SaleAgentCompID ?? 0;
+                    }
+
+                    $guid = (string) Str::uuid();
+
+                    $add_transaction -> objectType = $transaction -> listing;
+                    $add_transaction -> listingGuid = $guid;
+                    $add_transaction -> status = $transaction -> MlsStatus;
+                    $add_transaction -> listingDate = $transaction -> MLSListDate;
+                    $add_transaction -> expirationDate = $transaction -> ExpirationDate;
+                    $add_transaction -> escrowClosingDate = $transaction -> CloseDate;
+                    $add_transaction -> actualClosingDate = $transaction -> CloseDate;
+                    $add_transaction -> contractAcceptanceDate = $transaction -> PurchaseContractDate;
+                    $add_transaction -> listingPrice = $transaction -> ListPrice;
+                    $add_transaction -> salePrice = $transaction -> ClosePrice;
+
+                    $county = $transaction -> County;
+                    if($county == '') {
+                        $county = $this -> county(substr($transaction -> PostalCode, 0, 5));
+                    }
+                    $county = str_replace(' County', '', $county);
+                    $county = str_replace("'", "", $county);
+
+                    $property = [
+                        'zip' => $transaction -> PostalCode,
+                        'city' => $transaction -> City,
+                        'unit' => $transaction -> UnitNumber,
+                        'state' => $transaction -> StateOrProvince,
+                        'county' => $county,
+                        'direction' => $transaction -> StreetDirPrefix.$transaction -> StreetDirSuffix,
+                        'yearBuilt' => $transaction -> YearBuilt,
+                        'streetNumber' => $transaction -> StreetNumber,
+                        'streetAddress' => $transaction -> StreetName,
+                    ];
+                    $add_transaction -> property = json_encode($property);
+
+                    $agent = [
+                        'guid' => '',
+                        'publicId' => $agent_id
+                    ];
+                    $add_transaction -> agent = json_encode($agent);
+
+                    $add_transaction -> mlsNumber = $transaction -> ListingSourceRecordId ?? '0';
+                    $add_transaction -> data_source = 'mls_company';
+
+                    $add_transaction -> save();
+
+                    // add docs to db - downloading done in separate controller
+                    $dir = 'doc_management/skyslope/'.$guid;
+                    if(!Storage::exists($dir)) {
+                        Storage::makeDirectory($dir);
+                    }
+
+                    $downloads = [];
+
+                    foreach($docs as $doc) {
+
+                        if($doc -> invalid != 'yes') {
+
+                            //if(file_exists($doc -> upload_loc)) {
+
+                                $downloads[] = ['from' => $doc -> upload_loc, 'to' => $dir.'/'.$doc -> upload_file_name];
+
+                                $add_docs = new SkySlopeDocuments();
+                                $add_docs -> id = (string) Str::uuid();
+                                $add_docs -> listingGuid = $guid;
+                                $add_docs -> doc_type = $doc -> doc_type;
+                                $add_docs -> name = str_replace('.pdf', '', $doc -> upload_file_name);
+                                $add_docs -> fileName = $doc -> upload_file_name;
+                                $add_docs -> extension = 'pdf';
+                                $add_docs -> url = $doc -> upload_loc;
+                                $add_docs -> file_location = $dir.'/'.$doc -> upload_file_name;
+
+                                $add_docs -> save();
+
+                            //}
+
+                        }
+
+                    }
+
+                    if(count($downloads) > 0) {
+
+                        foreach($downloads as $download) {
+
+                            if(@fopen($download['from'], 'r')) {
+                                Storage::put($download['to'], file_get_contents($download['from']));
+                            }
+
+                        }
+
+                    }
+
+                }
+
+                $progress_increment = round((1 / count($transactions)) * 100);
+                $progress += $progress_increment;
+                $this -> queueProgress($progress);
+
+                $this -> queueData(['ListingSourceRecordIds' => $queue_data]);
+
+            }
 
         }
 
