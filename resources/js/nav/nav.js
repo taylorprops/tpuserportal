7e58a31bf8b14c289100b57c8ab87ab8f57f05bf
mike-taylorprops <mike@taylorprops.com>
2021-08-25 16:18:58 -0400

    changes

--- resources/js/nav/nav.js
+++ resources/js/nav/nav.js
@@ -1,14 +1,9 @@
 
 window.nav = function() {
     return {
-<<<<<<< HEAD
-        mobile: window.outerWidth < 1280,
-        main_nav_open: window.outerWidth >= 1280
-=======
         main_nav_open: window.outerWidth >= 1280,
         active_menu: '',
         active_sub_menu: ''
->>>>>>> nav
     }
 }
 
