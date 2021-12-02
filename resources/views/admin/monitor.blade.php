f2535493c462bfbdae318f1aeae575edb39d70e6
mike-taylorprops <mike@taylorprops.com>
2021-12-02 09:18:36 -0500

    daily changes

--- resources/views/admin/monitor.blade.php
+++ resources/views/admin/monitor.blade.php
@@ -9,20 +9,20 @@
         <div class="py-4 pl-6">
             <div class="flex justify-start items-center" x-data="controls()">
                 <div class="mr-2">
-                    <a href="javascript:void(0)" class="p-2 border rounded shadow control-button"
+                    <a href="javascript:void(0)" class="button lg no-text control-button"
                     x-on:click="active = '1'; play()"
-                    x-bind:class="{'bg-primary text-white': active === '1','bg-gray-100 text-gray-600': active !== '1' }">
+                    x-bind:class="{'secondary': active === '1','primary': active !== '1' }">
                         <i class="fal fa-play fa-lg"></i>
                     </a>
                 </div>
                 <div class="mr-2">
-                    <a href="javascript:void(0)" class="p-2 border rounded shadow control-button"
+                    <a href="javascript:void(0)" class="button primary lg no-text control-button"
                     x-on:click="active = '2'; stop()">
                         <i class="fal fa-stop fa-lg"></i>
                     </a>
                 </div>
                 <div class="mr-2">
-                    <a href="javascript:void(0)" class="p-2 border rounded shadow control-button"
+                    <a href="javascript:void(0)" class="button primary lg no-text control-button"
                     x-on:click="active = '3'; refresh()">
                         <i class="fal fa-redo fa-lg"></i>
                     </a>
