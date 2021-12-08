db1112764eaadec14f0c3687d9680daf867e326a
mike-taylorprops <mike@taylorprops.com>
2021-12-08 08:21:52 -0500

    daily changes

--- app/Jobs/OldDB/SkySlope/AddSkySlopeListingsJob.php
+++ app/Jobs/OldDB/SkySlope/AddSkySlopeListingsJob.php
@@ -51,7 +51,7 @@ class AddSkySlopeListingsJob implements ShouldQueue
             'Session' => $session
         ];
 
-        $modifiedAfter = str_replace(' ', 'T', date('Y-m-d H:i:s', strtotime('-1 day')));
+        $modifiedAfter = str_replace(' ', 'T', date('Y-m-d H:i:s', strtotime('-10 day')));
         //$modifiedBefore = str_replace(' ', 'T', date('Y-m-d H:i:s', strtotime('+1 day')));
 
         $query = [
