7dc90b5dec639c43488b31b9554697dc94566b97
mike-taylorprops <mike@taylorprops.com>
2021-12-12 08:28:49 -0500

    daily changes

--- app/Jobs/Cron/BrightMLS/UpdateOfficesJob.php
+++ app/Jobs/Cron/BrightMLS/UpdateOfficesJob.php
@@ -53,7 +53,7 @@ class UpdateOfficesJob implements ShouldQueue
         $resource = 'Office';
         $class = 'Office';
 
-        $mod_time = date('Y-m-d H:i:s', strtotime('-12 hour'));
+        $mod_time = date('Y-m-d H:i:s', strtotime('-80 hour'));
         $mod_time = str_replace(' ', 'T', $mod_time);
         $query = '(ModificationTimestamp='.$mod_time.'+)';
 
