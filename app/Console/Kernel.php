f9ace4549565d4ed64a035b93e646807db7369f1
mike-taylorprops <mike@taylorprops.com>
2021-12-12 16:05:25 -0500

    daily changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,15 +30,11 @@ class Kernel extends ConsoleKernel
         // prune failed jobs
         $schedule -> command('php artisan queue:prune-failed --hours=24') -> dailyAt('01:00');
         // update bright mls agents
-        /* $schedule -> command('bright_mls:update_agents') -> hourlyAt(3) -> environments('production');
+        $schedule -> command('bright_mls:update_agents') -> hourlyAt(3) -> environments('production');
         // update bright mls offices
         $schedule -> command('bright_mls:update_offices') -> hourlyAt(13) -> environments('production');
         // remove bright mls agents
-        $schedule -> command('bright_mls:remove_agents') -> everyFiveMinutes() -> environments('production'); */
-
-        $schedule -> command('bright_mls:update_agents') -> everyMinute() -> environments('production');
-        $schedule -> command('bright_mls:update_offices') -> everyMinute() -> environments('production');
-        $schedule -> command('bright_mls:remove_agents') -> everyMinute() -> environments('production');
+        $schedule -> command('bright_mls:remove_agents') -> everyFiveMinutes() -> environments('production');
 
         // %%% TEMP %%% //
 
