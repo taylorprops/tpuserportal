09e83f67b22102cc10339f201903f20895d87b98
mike-taylorprops <mike@taylorprops.com>
2021-12-22 10:07:22 -0500

    daily changes

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -31,7 +31,7 @@ class AuthenticatedSessionController extends Controller
     {
 
         if (!Auth::attempt(['email' => $request -> email, 'password' => $request -> password, 'active' => 'yes'])) {
-            return redirect('/') -> with('status', 'not active');
+            return response() -> json(['status' => 'not active']);
         }
         $request -> authenticate();
 
