8b1ba382b936739fbca3223a4ee7b4c1e83c5dd5
mike-taylorprops <mike@taylorprops.com>
2021-12-29 17:22:28 -0500

    daily changes

--- app/Http/Controllers/API/APIController.php
+++ app/Http/Controllers/API/APIController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\HeritageFinancial\Loans;
 
 class APIController extends Controller
 {
