5f21a564ec4d06619e0405f33e6c09dc25574496
mike-taylorprops <mike@taylorprops.com>
2021-12-29 20:00:24 -0500

    daily changes

--- routes/api.php
+++ routes/api.php
@@ -19,8 +19,6 @@ Route::middleware('auth:api') -> get('/user', function (Request $request) {
     return $request -> user();
 });
 
-Route::get('/api/lending_pad/check_loan_exists', [APIController::class, 'check_loan_exists']);
-
 Route::middleware('auth:api') -> get('/api/lending_pad/check_loan_exists', [APIController::class, 'check_loan_exists']);
 
 
