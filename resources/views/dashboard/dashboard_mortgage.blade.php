033f36755d5dc92f29fff3232ebe25fcc6e5b624
mike-taylorprops <mike@taylorprops.com>
2022-01-04 14:33:56 -0500

    daily changes

--- resources/views/dashboard/dashboard_mortgage.blade.php
+++ resources/views/dashboard/dashboard_mortgage.blade.php
@@ -45,7 +45,7 @@ $breadcrumbs = [];
                                 <div class="col-span-3 pl-2">
                                     {!! $borrower !!}
                                 </div>
-                                <div class="col-span-4 pl-2">
+                                <div class="col-span-4 pl-2 overflow-hidden">
                                     {!! $address !!}
                                 </div>
                                 <div class="col-span-2 -pl-2">
@@ -93,7 +93,7 @@ $breadcrumbs = [];
                                 <div class="col-span-3 pl-2">
                                     {!! $borrower !!}
                                 </div>
-                                <div class="col-span-4 pl-2">
+                                <div class="col-span-4 pl-2 overflow-hidden">
                                     {!! $address !!}
                                 </div>
                                 <div class="col-span-2 -pl-2">
