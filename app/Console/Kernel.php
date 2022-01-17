47a5f5c7cbdda0a20d5c85337801044fce963e9d
mike-taylorprops <mike@taylorprops.com>
2022-01-17 12:07:44 -0500

    daily changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,8 +27,8 @@ class Kernel extends ConsoleKernel
     {
 
         // Backups
-        $schedule -> command('backup:clean') -> daily() -> at('01:00') -> environments(['production']);
-        $schedule -> command('backup:run') -> daily() -> at('01:30') -> environments(['production']);
+        $schedule -> command('backup:clean') -> twiceDaily(1, 13) -> environments(['production']);
+        $schedule -> command('backup:run') -> twiceDaily(2, 14) -> environments(['production']);
 
         // prune failed jobs
         $schedule -> command('php artisan queue:prune-failed --hours=24') -> dailyAt('12:00');
