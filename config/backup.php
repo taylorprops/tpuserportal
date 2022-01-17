4610e42ab501a03f24e8df9785f69f7f3566e355
mike-taylorprops <mike@taylorprops.com>
2022-01-17 11:29:21 -0500

    daily changes

--- config/backup.php
+++ config/backup.php
@@ -149,7 +149,7 @@ return [
 
             'from' => [
                 'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-                'name' => env('MAIL_FROM_NAME', 'Example'),
+                'name' => '',
             ],
         ],
 
