456d7861e5d315797c5eadb1928221bf3307564e
mike-taylorprops <mike@taylorprops.com>
2022-02-07 16:25:09 -0500

    daily changes

--- app/Http/Controllers/API/APIController.php
+++ app/Http/Controllers/API/APIController.php
@@ -12,7 +12,7 @@ class APIController extends Controller {
     public function update_loan(Request $request) {
 
         $data = json_decode($request -> data);
-        return $request -> data;
+        return 'qoekinf';
 
         $lending_pad_id = $request -> loan_id;
         $address = Helper::parse_address_google('777 7th St NW #310 Washington, D.C., DC 20001');
