91dfcd80c100592cf3c14dbebfb207d2e6ac3eba
mike-taylorprops <mike@taylorprops.com>
2022-02-07 10:25:35 -0500

    daily changes

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,6 +16,7 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
+        return 'xxxxxxxx';
         return $next($request)
             -> header('Access-Control-Allow-Origin', '*')
             -> header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
