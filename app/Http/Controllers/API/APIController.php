189abba2ba4e255f162886d0b7186cd93de40005
mike-taylorprops <mike@taylorprops.com>
2022-02-08 12:37:58 -0500

    daily changes

--- app/Http/Controllers/API/APIController.php
+++ app/Http/Controllers/API/APIController.php
@@ -59,10 +59,7 @@ class APIController extends Controller {
         $co_borrower_first = $co_borrower['first'];
         $co_borrower_last = $co_borrower['last'];
 
-        return [
-            'borrower_first' => $borrower_first,
-            'borrower_last' => $borrower_last,
-        ];
+        return $borrower_first;
         $loan = Loans::find($lending_pad_id);
 
         if ($loan) {
