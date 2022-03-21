2b262c0adbf7b9ddc9f3ad28ad3d460c7436d1dc
mike-taylorprops <mike@taylorprops.com>
2022-03-21 09:19:19 -0400

    daily changes

--- resources/views/admin/system_monitor.blade.php
+++ resources/views/admin/system_monitor.blade.php
@@ -124,50 +124,48 @@ $breadcrumbs = [
                                 $job_name = substr($job -> name, (strrpos($job -> name, '\\') + 1));
                                 @endphp
 
-                                <div class="grid grid-cols-5 gap-4 border-b p-2 text-xs">
-                                    <div class="">
-                                        <div class="font-semibold">{{ $job_name }}</div>
-                                        <div>{{ $job -> queue }}</div>
-                                    </div>
-                                    <div>
-                                        Attempts:<br>
-                                        {{ $job -> attempt }}
-                                    </div>
-                                    <div>
-                                        Start:<br>
-                                        {{ $job -> started_at }}
-                                    </div>
-                                    <div>
-                                        End:<br>
-                                        {{ $job -> finished_at }}
-                                    </div>
-                                    <div class="w-600-px">
-                                        <div x-data="{ show_details: false }">
-                                            <div>
-                                                <a href="javascript: void(0)"
-                                                @click="show_details = true">
-                                                    Exceptions
-                                                    <i class="fal fa-arrow-right ml-2"></i>
-                                                </a>
-                                            </div>
-                                            <div x-show="show_details === false" x-transition>
-                                                {{ Str::limit($job -> exception_class, 150, $end = '...') }}
-                                            </div>
-                                            <div x-show="show_details === true" x-transition>
-                                                <div class="grid grid-cols-2 gap-4">
-                                                    <div>Excpetion Class</div>
-                                                    <div>{{ $job -> exception_class }}</div>
-                                                    <div>Excpetion</div>
-                                                    <div>{{ $job -> exception }}</div>
-                                                    <div>Excpetion Message</div>
-                                                    <div>{{ $job -> exception_message }}</div>
-                                                    <div>Data</div>
-                                                    <div>{{ $job -> data }}</div>
-                                                </div>
-                                            </div>
+                                <div class="border-b p-2 text-xs" x-data="{ show_details: false }">
+
+                                    <div class="grid grid-cols-7 gap-4">
+                                        <div>
+                                            <a href="javascript:void(0)" @click="show_details = true" x-show="show_details === false">View Details <i class="fal fa-arrow-right ml-2"></i></a>
+                                            <a href="javascript:void(0)" @click="show_details = false" x-show="show_details === true">Hide Details <i class="fal fa-arrow-down ml-2"></i></a>
+                                        </div>
+                                        <div>
+                                            <div class="font-semibold">{{ $job_name }}</div>
+                                            <div>{{ $job -> queue }}</div>
+                                        </div>
+                                        <div>
+                                            Attempts:<br>
+                                            {{ $job -> attempt }}
+                                        </div>
+                                        <div>
+                                            Start:<br>
+                                            {{ $job -> started_at }}
+                                        </div>
+                                        <div>
+                                            End:<br>
+                                            {{ $job -> finished_at }}
+                                        </div>
+                                        <div class="col-span-2">
+                                            Exception:<br>
+                                            {{ $job -> exception_class }}
                                         </div>
                                     </div>
+
+                                    <div class="grid grid-cols-6 rounded bg-gray-50 p-2" x-show="show_details === true" x-transition>
+                                        <div class="mb-4">Excpetion Class</div>
+                                        <div class="col-span-5 max-h-100-px overflow-auto mb-4">{{ $job -> exception_class }}</div>
+                                        <div class="mb-4">Excpetion Message</div>
+                                        <div class="col-span-5 max-h-100-px overflow-auto mb-4">{{ $job -> exception_message }}</div>
+                                        <div class="mb-4">Excpetion</div>
+                                        <div class="col-span-5 max-h-100-px overflow-auto mb-4">{!! preg_replace('/(#[0-9]+)/', '<br>$1', $job -> exception) !!}</div>
+                                        <div class="">Data</div>
+                                        <div class="col-span-5 max-h-100-px overflow-auto mb-4">{{ $job -> data }}</div>
+                                    </div>
+
                                 </div>
+
                             @endforeach
 
                         </div>
