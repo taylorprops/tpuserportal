0217377b8cc899bc749ea8b8cf6a955acb9edcaf
mike-taylorprops <mike@taylorprops.com>
2022-03-22 12:40:23 -0400

    daily changes

--- app/Http/Controllers/TestsController.php
+++ app/Http/Controllers/TestsController.php
@@ -36,8 +36,8 @@ class TestsController extends Controller
     public function test(Request $request) {
 
 
-        //exec("rsync -chavzP -e 'ssh -o StrictHostKeyChecking=no'  --delete --ignore-existing --stats /mnt/vol2/backups/ --exclude 'scripts' mike@162.244.66.22:/mnt/sdb/storage/mysql 2>&1", $output);
-        exec("cd /mnt && ls 2>&1", $output);
+        exec("rsync -chavzP -e 'ssh -o StrictHostKeyChecking=no'  --delete --ignore-existing --stats /mnt/vol2/backups/ --exclude 'scripts' mike@162.244.66.22:/mnt/sdb/storage/mysql 2>&1", $output);
+        //exec("cd /mnt && ls 2>&1", $output);
         dump($output);
         $rsync = new Rsync;
         $rsync -> site = 'All';
