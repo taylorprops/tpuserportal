346dc3bf4873b7cb8dfde4060f0be4d8c1a944cb
mike-taylorprops <mike@taylorprops.com>
2022-04-07 08:43:46 -0400

    daily changes

--- app/Jobs/Cron/BrightMLS/UpdateListingsJob.php
+++ app/Jobs/Cron/BrightMLS/UpdateListingsJob.php
@@ -49,7 +49,7 @@ class UpdateListingsJob implements ShouldQueue
 
                 $max_mod_timestamp = BrightListings::max('ModificationTimestamp');
 
-                $start = date("Y-m-d H:i:s", strtotime("$max_mod_timestamp -4 hour"));
+                $start = date("Y-m-d H:i:s", strtotime("$max_mod_timestamp -24 hour"));
                 $start = str_replace(' ', 'T', $start);
                 $this -> queueData(['start:' => $start], true);
 
