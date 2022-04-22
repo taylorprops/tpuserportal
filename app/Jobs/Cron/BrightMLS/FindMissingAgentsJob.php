6e3ea3de38eda490f1f0fd2f2fcd3141ce9af7bc
mike-taylorprops <mike@taylorprops.com>
2022-04-22 11:24:47 -0400

    daily changes

--- app/Jobs/Cron/BrightMLS/FindMissingAgentsJob.php
+++ app/Jobs/Cron/BrightMLS/FindMissingAgentsJob.php
@@ -2,9 +2,11 @@
 
 namespace App\Jobs\Cron\BrightMLS;
 
+use App\Helpers\Helper;
 use Illuminate\Bus\Queueable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
+use App\Models\BrightMLS\BrightAgentRoster;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
