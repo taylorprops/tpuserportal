8168bf3cb5299591128454f284d4f127e26a33a2
mike-taylorprops <mike@taylorprops.com>
2022-05-04 21:13:06 -0400

    daily changes

--- resources/views/API/zoho/get_bright_agent_details_html.blade.php
+++ resources/views/API/zoho/get_bright_agent_details_html.blade.php
@@ -1,5 +1,6 @@
-<div class="p-12">
-    <div class="my-6 text-xl font-semibold text-gray-600">BrightMLS Details</div>
+<div class="p-12 text-3xl">
+
+    <div class="my-6 text-3xl font-semibold text-gray-600">BrightMLS Details</div>
 
     <div class="">
         {{ $agent -> MemberFullName }}
