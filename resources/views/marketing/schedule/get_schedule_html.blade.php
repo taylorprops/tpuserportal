2bc4eb592e7cd27f58bc17ac37a93d1557dbe7b2
mike-taylorprops <mike@taylorprops.com>
2022-05-24 14:13:51 -0400

    daily changes

--- resources/views/marketing/schedule/get_schedule_html.blade.php
+++ resources/views/marketing/schedule/get_schedule_html.blade.php
@@ -146,7 +146,7 @@ if($event -> event_date < date('Y-m-d') && $event -> status -> item != 'Complete
 
                     <div class="p-2 border rounded-lg shadow-md">
 
-                        <div class="w-full notes-div max-h-500-px overflow-auto pr-4" data-id="{{ $event -> id }}"></div>
+                        <div class="max-w-600-px mx-auto notes-div max-h-500-px overflow-auto pr-4" data-id="{{ $event -> id }}"></div>
 
                     </div>
 
