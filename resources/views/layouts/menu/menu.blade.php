9f3ef4b9b3b492a7503e0af83e4f8348f470b865
mike-taylorprops <mike@taylorprops.com>
2022-06-06 10:13:12 -0400

    daily changes

--- resources/views/layouts/menu/menu.blade.php
+++ resources/views/layouts/menu/menu.blade.php
@@ -1,28 +1,28 @@
 <div class="fixed top-0 left-0 h-screen overflow-y-auto xl:overflow-y-none z-10"
-@click.outside="if(screen.width < 1000) { main_nav_open = false }">
+    @click.outside="if(screen.width < 1000) { main_nav_open = false }">
 
     <div class="fixed top-0 left-0 z-100">
         <div class="h-16 flex items-center">
             <button class="py-2 px-4 sm:py-3"
-            @click="main_nav_open = !main_nav_open;"
-            x-show="!main_nav_open" x-transition:enter.duration.500ms>
+                @click="main_nav_open = !main_nav_open;"
+                x-show="!main_nav_open" x-transition:enter.duration.500ms>
                 <i class="fal fa-bars fa-lg text-gray-600"></i>
             </button>
             <button class="py-2 px-4 sm:py-3"
-            @click="main_nav_open = !main_nav_open;"
-            x-show="main_nav_open" x-transition:enter.duration.500ms>
+                @click="main_nav_open = !main_nav_open;"
+                x-show="main_nav_open" x-transition:enter.duration.500ms>
                 <i class="fal fa-times fa-lg text-gray-600"></i>
             </button>
         </div>
     </div>
 
     <div class="relative w-64 h-full border-r shadow z-10" x-show="main_nav_open"
-    x-transition:enter="transition ease-in-out duration-300"
-    x-transition:enter-start="opacity-0 transform scale-x-0 -translate-x-1/2"
-    x-transition:enter-end="opacity-100 transform scale-x-100 translate-x-0"
-    x-transition:leave="transition ease-in-out duration-300"
-    x-transition:leave-start="opacity-100 transform scale-x-100 translate-x-0"
-    x-transition:leave-end="opacity-0 transform scale-x-0 -translate-x-1/2">
+        x-transition:enter="transition ease-in-out duration-300"
+        x-transition:enter-start="opacity-0 transform scale-x-0 -translate-x-1/2"
+        x-transition:enter-end="opacity-100 transform scale-x-100 translate-x-0"
+        x-transition:leave="transition ease-in-out duration-300"
+        x-transition:leave-start="opacity-100 transform scale-x-100 translate-x-0"
+        x-transition:leave-end="opacity-0 transform scale-x-0 -translate-x-1/2">
 
 
         <div class="flex flex-col justify-between bg-gray-50 h-full w-64">
@@ -37,7 +37,7 @@
 
                 <div class="hidden sm:block my-2 sm:mb-0 mt-4 mx-auto max-w-200-px relative">
                     <input class="main-search-input form-element input sm" type="text" placeholder="Search"
-                    @keyup.debounce="search($el)">
+                        @keyup.debounce="search($el)">
                     <i class="fal fa-search absolute top-2 sm:top-2 right-3"></i>
                 </div>
 
@@ -48,8 +48,14 @@
                 <div class="">
 
                     <nav class="navigation pl-2 pt-3 pb-2 bg-gray-50 border-t h-full"
-                    x-data="{ close_all: false }">
-                        @include('layouts/menu/menu_'.auth() -> user() -> group)
+                        x-data="{ close_all: false }">
+                        @php
+                            $group = auth() -> user() -> group;
+                            if (auth() -> user() -> level == 'owner') {
+                                $group = 'owner';
+                            }
+                        @endphp
+                        @include('layouts/menu/menu_' . $group)
                     </nav>
 
                 </div>
@@ -67,4 +73,3 @@
     </div>
 
 </div>
-
