6d130481fbdc629d74f3d4e00e3b77c110a5ccdd
mike-taylorprops <mike@taylorprops.com>
2022-06-17 08:51:53 -0400

    daily changes

--- app/Jobs/Cron/Schedule/NotifySendingJob.php
+++ app/Jobs/Cron/Schedule/NotifySendingJob.php
@@ -3,8 +3,11 @@
 namespace App\Jobs\Cron\Schedule;
 
 use Illuminate\Bus\Queueable;
+use App\Mail\General\EmailGeneral;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
+use App\Models\Marketing\Schedule\Schedule;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
@@ -48,46 +51,50 @@ class NotifySendingJob implements ShouldQueue
 
             $this -> queueData(['Found '.count($events).' events'], true);
 
-            foreach($events as $event) {
+            if(count($events) > 0) {
 
-                switch ($event -> company_id) {
-                    case '1': // TP
-                        $tos = config('global.marketing_email_notification_TP');
-                        break;
-                    case '2': // HF
-                        $tos = config('global.marketing_email_notification_HF');
-                        break;
-                    case '3': // HT
-                        $tos = config('global.marketing_email_notification_HT');
-                        break;
+                foreach($events as $event) {
+
+                    switch ($event -> company_id) {
+                        case '1': // TP
+                            $tos = config('global.marketing_email_notification_TP');
+                            break;
+                        case '2': // HF
+                            $tos = config('global.marketing_email_notification_HF');
+                            break;
+                        case '3': // HT
+                            $tos = config('global.marketing_email_notification_HT');
+                            break;
+                    }
+
+
+                    $html = $event -> uploads -> first() -> html;
+                    $details = 'Send Date: '.$event -> event_date.'<br>
+                    From: '.$event -> company -> item.'<br>
+                    To: '.$event -> recipient -> item.'<br>
+                    ID: '.$event -> id;
+
+                    $body = preg_replace('/(<body\s.*>)/', '$1'.$details, $html);
+
+
+                    $message = [
+                        'company' => 'Taylor Properties',
+                        'subject' => 'Marketing Email - '.$event -> subject_line_a,
+                        'from' => ['email' => 'internal@taylorprops.com', 'name' => 'Taylor Properties'],
+                        'body' => $body,
+                        'attachments' => null
+                    ];
+
+                    $tos = ['miketaylor0101@gmail.com'];
+                    Mail::to($tos)
+                        -> send(new EmailGeneral($message));
+
+                    $event -> notification_sent = true;
+                    $event -> save();
+
+                    $this -> queueData([$event -> id.' - Completed'], true);
+
                 }
-
-
-                $html = $event -> uploads -> first() -> html;
-                $details = 'Send Date: '.$event -> event_date.'<br>
-                From: '.$event -> company -> item.'<br>
-                To: '.$event -> recipient -> item.'<br>
-                ID: '.$event -> id;
-
-                $body = preg_replace('/(<body\s.*>)/', '$1'.$details, $html);
-
-
-                $message = [
-                    'company' => 'Taylor Properties',
-                    'subject' => 'Marketing Email - '.$event -> subject_line_a,
-                    'from' => ['email' => 'internal@taylorprops.com', 'name' => 'Taylor Properties'],
-                    'body' => $body,
-                    'attachments' => null
-                ];
-
-                $tos = ['miketaylor0101@gmail.com'];
-                Mail::to($tos)
-                    -> send(new EmailGeneral($message));
-
-                $event -> notification_sent = true;
-                $event -> save();
-
-                $this -> queueData([$event -> id.' - Completed'], true);
 
             }
 
