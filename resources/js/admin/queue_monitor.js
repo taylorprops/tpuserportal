65268dee9d2d829a0795d38141469540bb9a87be
mike-taylorprops <mike@taylorprops.com>
2022-07-05 14:55:45 -0400

    daily changes

--- resources/js/admin/queue_monitor.js
+++ resources/js/admin/queue_monitor.js
@@ -8,43 +8,43 @@ if (document.URL.match(/queue_monitor/)) {
         let type = global_get_url_parameters('type') || '';
         let queue = global_get_url_parameters('queue') || '';
 
-        axios.get('/jobs?type='+type+'&queue='+queue+'&page='+page)
-        .then(function (response) {
+        axios.get('/jobs?type=' + type + '&queue=' + queue + '&page=' + page)
+            .then(function (response) {
 
-            document.querySelector('.monitor').innerHTML = response.data;
+                document.querySelector('.monitor').innerHTML = response.data;
 
-            let monitor = document.querySelector('.monitor');
-            //console.log(monitor.offsetLeft);
-            monitor.querySelector('[charset="utf-8"]').remove();
-            monitor.querySelector('[name="viewport"]').remove();
-            monitor.querySelector('title').remove();
-            let link = monitor.getElementsByTagName('link')[0];
-            link.remove();
-            let h1 = monitor.getElementsByTagName('h1')[0];
-            h1.remove();
+                let monitor = document.querySelector('.monitor');
+                //console.log(monitor.offsetLeft);
+                monitor.querySelector('[charset="utf-8"]').remove();
+                monitor.querySelector('[name="viewport"]').remove();
+                monitor.querySelector('title').remove();
+                let link = monitor.getElementsByTagName('link')[0];
+                link.remove();
+                let h1 = monitor.getElementsByTagName('h1')[0];
+                h1.remove();
 
-            // document.querySelector('.monitor').innerHTML = '';
-            // document.querySelector('.monitor').insertAdjacentHTML('beforeend', response.data);
+                // document.querySelector('.monitor').innerHTML = '';
+                // document.querySelector('.monitor').insertAdjacentHTML('beforeend', response.data);
 
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
 
 
-    window.controls = function() {
+    window.controls = function () {
         return {
             play_interval: null,
             active: '0',
             init() {
                 let scope = this;
-                setTimeout(function() {
-                    scope.resize_textareas('16');
-                }, 1000);
+                // setTimeout(function() {
+                //     scope.resize_textareas('16');
+                // }, 1000);
             },
             play() {
-                if(this.play_interval) {
+                if (this.play_interval) {
                     clearInterval(this.play_interval);
                 }
                 this.play_interval = setInterval(jobs, 1000);
