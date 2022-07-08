fcbea28dffe24783fff292d7aa06989a2e0fd7f2
mike-taylorprops <mike@taylorprops.com>
2022-07-08 09:15:51 -0400

    daily changes

--- app/Jobs/Cron/BrightMLS/UpdateAgentsAndOfficesJob.php
+++ app/Jobs/Cron/BrightMLS/UpdateAgentsAndOfficesJob.php
@@ -249,7 +249,7 @@ class UpdateAgentsAndOfficesJob implements ShouldQueue
 
                         $add_agent = BrightAgentRoster::firstOrCreate(
                             ['MemberKey' => $MemberKey],
-                            $agent_details
+                            [$agent_details]
                         );
 
                     }
